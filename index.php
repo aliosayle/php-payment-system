94f499ffe48bd7e65f248bb1f31282615bbdb062
aliosseili <aliossaily24@gmail.com>
2024-01-07 10:36:08 +0200

    added the universal navbar

--- index.php
+++ index.php
@@ -133,9 +133,6 @@
     </style>
 </head>
 <body>
-    <header class="bg-primary text-white text-center p-2">
-        <h1>PayOne</h1>
-    </header>
     <?php
                 include "con.php"; 
                 session_start();
@@ -144,18 +141,7 @@
                 $sql_get_requests = "SELECT * FROM payment_requests WHERE payee_id = '$name' AND status = 'pending'";
                 $result_get_requests = $con->query($sql_get_requests);
     ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container">
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav text-center">
-                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Balance</a></li>
-                    <li class="nav-item"><a class="nav-link" href="transfer.php">Transfer</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">About us</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include "navbar.php"; ?>
 
     <?php
         if (!isset($_SESSION['username'])) {
